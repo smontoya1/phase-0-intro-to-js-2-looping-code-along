6db4e72fc64e17a6cf356df05c13521b4aa81e45
Shaun <smontoya0009@gmail.com>
2025-01-12 19:44:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+/*
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const occasion = "surprise";
+*/
+function writeCards (names, occasion) {
+    const message = [];
+    for(let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return message;
+}
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], "surprise");
+
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
